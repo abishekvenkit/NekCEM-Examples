039cdff0b19cc05f4d7026c745608d2ca64bfe8d
Abishek Venkit <abishekvenkit@users.noreply.github.com>
2017-07-18 08:51:47 -0500

    added new folder lens_rea to matlab_script folder containing scripts to read rea files and modify them to change radius and gap

---
+++ LENS/matlab_script/lens_rea/lens_mesh.m
@@ -0,0 +1,68 @@
+clear all
+close all
+
+rea_file = input('What is your .rea filename? (must located in same folder):  ','s');
+out_file = input('Name the output file:   ','s');
+
+new_rad = input('What is your new radius? (0.5 to keep the same)  ');
+new_gap = input('What is your new gap? (0.10435 to keep the same)  ');
+
+
+% Step 1: read .rea file
+data_old = read_rea(rea_file); % keep old version
+data = data_old;
+E = data_old.info.E; % number of the element
+
+
+% Step 2 Identify points <<<<<< main script
+rad = 0.5 %radius of original cylinder mesh
+gap = .10435 %gap of original cylinder mesh
+
+v = data.vertex;
+cir = 0*v;
+for e = 1:E;
+    for i = 1:4;
+        if (sqrt(v(e,i,1).^2+v(e,i,2).^2)-rad) < .01
+            cir(e,i) = 1;
+        else 
+            cir(e,i) = 0;
+        end
+    end
+end
+
+for e = 1:E;
+    for i = 1:4;
+        if cir(e,i) == 1
+            v(e,i,1) = (new_rad/rad)*v(e,i,1);
+            v(e,i,2) = (new_rad/rad)*v(e,i,2);
+        else
+            v(e,i,1) = (new_gap/gap)*v(e,i,1);
+            v(e,i,2) = (new_gap/gap)*v(e,i,2);
+        end
+    end
+end
+data.vertex = v;
+
+
+% Step 3: plotting
+figure(1)
+for e=1:E
+    v = data_old.vertex;
+    plot([v(e,:,1),v(e,1,1)],[v(e,:,2),v(e,1,2)],'ob-'); hold on
+    text(sum(v(e,:,1))/4,sum(v(e,:,2))/4,num2str(e))
+end
+title('old mesh')
+axis equal
+
+figure(2)
+for e=1:E
+    v = data.vertex;
+    plot([v(e,:,1),v(e,1,1)],[v(e,:,2),v(e,1,2)],'ob-'); hold on
+    text(sum(v(e,:,1))/4,sum(v(e,:,2))/4,num2str(e))
+end
+title('new mesh')
+axis equal
+
+% Step 4: output 
+write_rea(rea_file,out_file,data_old);
+
---
+++ LENS/matlab_script/lens_rea/write_rea.m
@@ -0,0 +1,104 @@
+function write_rea(ref_rea,out_rea,data)
+% This function generate .rea file based on an old rea file and modified data
+%
+% data.info.E:      num of element
+% data.info.ndim:   dimension
+% data.info.nelg:   nelg
+% data.info.vertex: (E, v1~v4, x or y)
+% data.str_rea:     '      ELEMENT          1 [    0a]    GROUP     0'
+% data.num_c:       num of special curves
+% data.bool_curve:  if is special curve, 1/0
+% data.edge_type:   m: mid point, c: circle
+% data.curve:       parameters of curves
+% data.BC_type:     type of BC, P/E/...
+% data.BC:          parameters of BC
+
+
+fid_ref = fopen(ref_rea,'r');
+fid_out = fopen(out_rea,'w');
+sec_num(1) = 143; % irrelative lines
+
+sp2 = '  ';
+sp4 = '    ';
+sp6 = '      ';
+
+fm2 = [sp2 '%.7f' sp6 '%.7f' sp6 '%.7f' sp6 '%.7f'];
+fm31 = [sp2 '%d  %d'];
+fm32 = [sp2 '%.5E' sp6 '%.5E' sp6 '%.5E' sp6 '%.5E' sp6 '%.5E'];
+fm33 = [sp6 '%c \n'];
+fm41 = [sp2 '%c'];
+fm42 = [sp4 '%d  %d'];
+fm43 = [sp4 '%.7f' sp6 '%.4f' sp6 '%.4f' sp6 '%.4f' sp6 '%.4f\n'];
+
+% Sec 1: go through sec 1
+for i=1:sec_num(1)
+    fline = fgets(fid_ref);
+    fprintf(fid_out,fline);
+end
+
+
+% Sec 2: read vertices of elements
+fline = fgets(fid_ref);
+fprintf(fid_out,fline);
+E = data.info.E;
+
+vertex = data.vertex;
+str_rea= data.str_rea; 
+for e=1:E
+    fline = fgets(fid_ref); % do nothing
+    fprintf(fid_out,char(str_rea(e)));
+    
+    fline = fgets(fid_ref); % do nothing
+    fprintf(fid_out,fm2,vertex(e,:,1)); 
+    fprintf(fid_out,'\n');
+    
+    fline = fgets(fid_ref); % do nothing
+    fprintf(fid_out,fm2,vertex(e,:,2));  
+    fprintf(fid_out,'\n');
+end
+
+% Sec 3: read spetial curves
+fline = fgets(fid_ref);fprintf(fid_out,fline);
+fline = fgets(fid_ref);fprintf(fid_out,fline);
+num_c = data.num_c;
+
+edge = data.edge_type;
+curve = data.curve;
+for i=1:E
+    for j=1:4
+        if data.bool_curve(i,j)
+            fline = fgets(fid_ref); % do nothing
+            
+            fprintf(fid_out,fm31,j,i);
+            fprintf(fid_out,fm32,curve(i,j,:));  
+            fprintf(fid_out,fm33,char(edge(i,j)));
+        end
+    end
+end
+
+
+% Sec 4: read boundary condition
+fline = fgets(fid_ref);fprintf(fid_out,fline);
+fline = fgets(fid_ref);fprintf(fid_out,fline);
+
+BC = data.BC;
+BC_type = data.BC_type;
+for i=1:E 
+    for j=1:4
+        fline = fgets(fid_ref); % do nothing
+        fprintf(fid_out,fm41,char(BC_type(i,j)));
+        fprintf(fid_out,fm42,i,j);  
+        fprintf(fid_out,fm43,BC(i,j,:));
+    end
+end
+
+fline = fgets(fid_ref);
+while fline ~= -1
+    fprintf(fid_out,fline);
+    fline = fgets(fid_ref);
+end
+
+fclose(fid_ref);
+end
+
+
